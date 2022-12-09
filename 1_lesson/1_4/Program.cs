c57941e71a06b2a53ff2f444d549d13e650c05f0
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-09 22:33:50 +0300

    task 1_4

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу вычислени модуля числа
+Console.WriteLine("Введите число: ");
+
+string Num = Console.ReadLine();
+int N = int.Parse(Num);
+
+if(N < 0)
+    Console.WriteLine(N*(-1));
+else
+   Console.WriteLine(N); 
