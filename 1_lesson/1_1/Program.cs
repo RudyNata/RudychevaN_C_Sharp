e6f79e8326ad629c8d4e7f4265317a55a4da7b71
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-09 20:44:27 +0300

    task 1_1

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// a=25, b=5
+Console.WriteLine("Write a number 1: ");
+
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number 2: ");
+
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 == num2*num2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
