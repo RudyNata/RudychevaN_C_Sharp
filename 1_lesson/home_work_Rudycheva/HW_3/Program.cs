3a16e24f8edd9f3c985bea5d1aee51fa71750dd5
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-11 00:25:28 +0300

    Home work 3

---
+++ 1_lesson/home_work_Rudycheva/HW_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число: ");
+string Num = Console.ReadLine();
+int N = int.Parse(Num);
+
+    
+if(N % 2 == 0)
+    Console.WriteLine("Четное");
+else
+    Console.WriteLine("Нечетное");
