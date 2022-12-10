dee33ee68692800f84cc0a905a8dfdb88c38ab5e
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-10 23:55:27 +0300

    Home work 1

---
+++ 1_lesson/home_work_Rudycheva/HW_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число 1: ");
+string Num1 = Console.ReadLine();
+int N1 = int.Parse(Num1);
+
+Console.WriteLine("Введите число 2: ");
+string Num2 = Console.ReadLine();
+int N2 = int.Parse(Num2);
+
+if(N1 > N2)
+    Console.WriteLine(N1);
+else
+   Console.WriteLine(N2); 
