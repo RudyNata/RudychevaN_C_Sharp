db5040704578eb5ff971b78e0490d90fb06766c4
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-19 02:33:29 +0300

    2 lesson 2_0

---
+++ 2_lesson/2_0/Program.cs
@@ -0,0 +1,10 @@
+﻿int MaxNumber(int num)
+{
+    Console.WriteLine(num);
+    if (num % 10 > num / 10)
+        return num % 10;
+    else
+        return num / 10;
+}
+
+Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
