42584c19710ed269e1247baec286f5137555e228
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-20 03:16:34 +0300

    task 2_3

---
+++ 2_lesson/2_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+string Crat(int num)
+{
+    if((num % 7 == 0)&(num % 23 == 0))
+        return "Кратно обоим числам";
+    else
+        return "Не кратно";
+}
+
+Console.WriteLine("Write number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Crat(number));
