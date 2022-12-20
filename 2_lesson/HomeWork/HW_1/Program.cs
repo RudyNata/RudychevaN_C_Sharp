49babed66f86dc60c159073a808cca687a29376d
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-21 01:06:33 +0300

    Task HW_1

---
+++ 2_lesson/HomeWork/HW_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+string SecondNum(int num)
+{
+    if((num > 99)&(num < 999))
+        return $"{(num / 10) % 10}";
+    else
+        return "Не трехзначное число";
+
+}
+
+Console.WriteLine("Write number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SecondNum(number));
