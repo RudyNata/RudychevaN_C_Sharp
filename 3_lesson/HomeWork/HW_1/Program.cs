07d7f9d34a4858e3ea7ae42242e87e42e7e2c2d0
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-23 02:50:55 +0300

    Task HW_1

---
+++ 3_lesson/HomeWork/HW_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+string Polindrom (int Num)
+{
+    if(Num / 10000 == Num % 10 && (Num / 1000) % 10 == (Num / 10) % 10);
+        return "Number is polindrom";
+    else
+        return "Not a five digit number";
+       
+}
+
+Polindrom(int.Parse(Console.ReadLine()));
