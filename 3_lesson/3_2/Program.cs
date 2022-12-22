edd344f4191fff2347e1a45f2a51208d66dfec8c
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-22 03:22:57 +0300

    Task 3_2

---
+++ 3_lesson/3_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D пространстве.
+
+double Distance(double x1, double y1, double x2, double y2)
+{
+    double Dist;
+    Dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    return Dist;
+}
+
+Console.Write("Введите координату х1: ");
+double CoordX1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите координату y1: ");
+double CoordY1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите координату х2: ");
+double CoordX2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите координату y2: ");
+double CoordY2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine(Distance(CoordX1, CoordY1, CoordX2, CoordY2));
