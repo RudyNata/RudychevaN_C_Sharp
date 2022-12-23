958e63412971a10a3d2acd451c839fe682ccc60c
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-23 03:09:30 +0300

    Task HW_3

---
+++ 3_lesson/HomeWork/HW_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void CubeNumber(int N)
+{
+    for(int i = 1; i <= N; i++)
+    {
+        double CubeNum = Math.Pow(i, 3);
+        Console.Write($"{CubeNum}, ");
+    }   
+}
+
+CubeNumber(int.Parse(Console.ReadLine()));
