0836c3c6c7ee6a3fae97c4af1c7ad208f35e412f
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-26 02:23:05 +0300

    Task 4_1 and 4_2

---
+++ 4_lesson/4_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+
+int NumOfDigits(int num)
+{
+   int number = 1;
+    for(int i = 1; num > 0; i++)
+     {
+        num = num / 10;
+        number = i;
+     }
+   return number;
+}
+
+Console.WriteLine(NumOfDigits(int.Parse(Console.ReadLine())));
---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int Factorial(int num)
+{
+    int fact = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+Console.WriteLine(Factorial(int.Parse(Console.ReadLine())));
