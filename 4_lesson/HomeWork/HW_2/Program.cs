982c1c36271df7470fee278cb33ba4c7c4c047be
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-26 03:10:08 +0300

    Task HW_2

---
+++ 4_lesson/HomeWork/HW_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Summ(int num)
+{
+    int Summa = 0;
+    for(int i = 1; num > 0; i++)
+     {
+        Summa = Summa + num % 10;
+        num = num / 10;
+     }
+     return Summa;
+}
+
+Console.Write("Write a number: ");
+Console.WriteLine(Summ(int.Parse(Console.ReadLine())));
