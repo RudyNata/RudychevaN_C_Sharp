7cd3e45f0b230be424792836d6ae1d2a0ddae523
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-26 03:02:57 +0300

    Task HW_1

---
+++ 4_lesson/HomeWork/HW_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Stepen(int num, int step)
+{
+    int Product = 1;
+    for (int i = 1; i <= step; i++)
+    {
+        Product = Product * num;
+    }
+    return Product;
+}
+
+Console.WriteLine("Введите число 1:");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2:");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Stepen(num: num1, step: num2));
