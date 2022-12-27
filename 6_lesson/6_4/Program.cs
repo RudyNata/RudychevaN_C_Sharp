e905e370516e99dfff2583e895021a74a0436cff
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-28 02:32:06 +0300

    Task 6_4

---
+++ 6_lesson/6_4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет создавать копию
+// заданного массива с помощью поэлементного копирования.
+
+void PrintArray(int[,] collection)
+{
+    int raw = collection.GetLength(0);
+    int stolb = collection.GetLength(1);
+
+    for (int i = 0; i < raw; i++)
+    {
+        for (int j = 0; j < stolb; j++)
+            Console.Write($"{collection[i,j]}, ");
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int raw, int stolb)
+{
+    int[,] array = new int[raw, stolb];
+    for (int i = 0; i < raw; i++)
+        for (int j = 0; j < stolb; j++)
+            array[i,j] = new Random().Next(-100, 100);
+    return array;
+}
+
+int[,] arr_1 = FillArray(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+PrintArray(arr_1);
