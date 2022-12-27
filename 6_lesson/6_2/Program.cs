ca79042688c0b14319d0c316791ad442b2138ee1
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-28 01:50:35 +0300

    Task 6_2

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+string Binary(int num)
+{
+    int rem_num = num;
+    string binary_num = "";
+    while (rem_num > 0)
+    {
+        binary_num = $"{rem_num % 2}" + binary_num;
+        rem_num = rem_num / 2;
+    }
+    return binary_num; ;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write(Binary(number));
+
