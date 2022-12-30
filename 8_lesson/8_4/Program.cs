47b41fd260e89040f637933ebcc6d22885b8b551
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-31 01:17:28 +0300

    Task 8_4

---
+++ 8_lesson/8_4/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайтедвумерный массив из целых чисел. Напишите программу,
+// которая удалит строку и столбец, на пересечении которых
+// расположен наименьший элемент массива.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i,j], 4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void MinNum(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    (int, int) min = (0, 0);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            {
+                if(arr[min.Item1, min.Item2] >= arr[i, j]);
+                    min = (i,j);
+            }
+    }
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            if(i == min.Item1 | j == min.Item2) continue;
+            Console. Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+Console.Write("Row: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Column: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] array = MassNums(row, column, 
+                            int.Parse(Console.ReadLine()),
+                            int.Parse(Console.ReadLine()));
+Print(array);
+MinNum(array);
