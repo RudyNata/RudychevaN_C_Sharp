bbcece3f4cbc4793be15cf08b29d29f280204bd4
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-30 23:33:21 +0300

    Task 7_1

---
+++ 8_lesson/8_1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте двумерный массив. Напишите программу, которая
+// поменяет местами первую и последнюю строку массива.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i,j], 4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int[,] NewArr(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            (arr[i,j], arr[row_size - 1, j]) = (arr[row_size - 1, j], arr[i,j]);
+        }
+    break;
+    }
+    return arr;
+}
+
+Console.Write("Row: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Column: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] array = MassNums(row, column, 
+                            int.Parse(Console.ReadLine()),
+                            int.Parse(Console.ReadLine()));
+Print(array);
+NewArr(array);
+Print(array);
