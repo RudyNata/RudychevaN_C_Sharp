8ae60f8fd176d47f39bb9be876e3d789543b605f
Rudycheva Natalia <rudy-nata@mail.ru>
2022-12-30 21:54:05 +0300

    Task HW_2

---
+++ 7_lesson/HomeWork/HW_2/Program.cs
@@ -0,0 +1,60 @@
+﻿// 2. Напишите программу, которая на вход принимает позиции
+// элемента в двумерном массиве, и возвращает значение этого
+// элемента или же указание, что такого элемента нет.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+string Search(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    int index1 = int.Parse(Console.ReadLine());
+    int index2 = int.Parse(Console.ReadLine());
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+           {
+            int search_num = arr[i,j];
+            if (i == index1 && j == index2)
+                return $"{search_num}";
+           }             
+    }
+    return "No number"; 
+}
+
+Console.Write("Row: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Column: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] array = MassNums(row, column,
+                            int.Parse(Console.ReadLine()),
+                            int.Parse(Console.ReadLine()));
+Print(array);
+Console.WriteLine((Search(array)));
+
